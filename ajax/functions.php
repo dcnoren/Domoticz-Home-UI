febe991fde0ac95c3b86d13de393a81ea431a0f1
dcnoren <dcnoren@gmail.com>
2015-12-04 16:17:56 -0500

    bf

--- ajax/functions.php
+++ ajax/functions.php
@@ -446,7 +446,7 @@ function setSceneStatus($scene){
 		do {
 			curl_multi_exec($mh, $running);
 		} while ($running);
-		curl_close($mh);
+		curl_close($cm);
 		
 	} elseif ($scene == "2"){
 		setDimmerStatus("7", "On", "false");	//Down Hall
