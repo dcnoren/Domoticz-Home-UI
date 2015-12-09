ce33364d5827f4c24063f714db122a8d441a09d7
dcnoren <dcnoren@gmail.com>
2015-12-08 21:07:04 -0500

    debugging

--- task.php
+++ task.php
@@ -7,8 +7,10 @@ $comfort = $getAllStatus["comfort"];
 
 if (($comfort["43"] - $comfort["15"]) >= 10){
 	setStatus("41", "On");
+	echo "41 on";
 } elseif (($comfort["43"] - $comfort["15"]) <= 10){
 	setStatus("41", "Off");
+	echo "41 off";
 }
 
 ?>
