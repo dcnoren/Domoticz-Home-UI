3bf5975805ff58232dad4403ff64ebf54898d5be
dcnoren <dcnoren@gmail.com>
2015-12-08 21:47:25 -0500

    fixed conflict issue

--- task.php
+++ task.php
@@ -7,7 +7,7 @@ if (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]
 	echo "43 is " . $getAllStatus["comfort"]["43"]["Humidity"] . " and 15 is " . $getAllStatus["comfort"]["15"]["Humidity"];
 	setStatus("41", "On");
 	echo "<br />41 on";
-} elseif (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) <= 15){
+} elseif (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) < 15){
 	echo "43 is " . $getAllStatus["comfort"]["43"]["Humidity"] . " and 15 is " . $getAllStatus["comfort"]["15"]["Humidity"];
 	setStatus("41", "Off");
 	echo "<br />41 off";
