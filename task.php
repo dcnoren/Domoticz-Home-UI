6a34024d9e86e620419a28dd3f8ceb3da5d69d7f
dcnoren <dcnoren@gmail.com>
2015-12-08 21:17:05 -0500

    aa

--- task.php
+++ task.php
@@ -3,14 +3,18 @@ require('ajax/functions.php');
 
 $getAllStatus = getAllStatus();
 
+print_r $getAllStatus["comfort"]["15"];
+
+echo "<br />";
+
 if (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) >= 10){
 	echo "43 is " . $getAllStatus["comfort"]["43"]["Humidity"] . " and 15 is " . $getAllStatus["comfort"]["15"]["Humidity"];
 	setStatus("41", "On");
-	echo "</br>41 on";
+	echo "<br />41 on";
 } elseif (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) <= 10){
 	echo "43 is " . $getAllStatus["comfort"]["43"]["Humidity"] . " and 15 is " . $getAllStatus["comfort"]["15"]["Humidity"];
 	setStatus("41", "Off");
-	echo "</br>41 off";
+	echo "<br />41 off";
 }
 
 ?>
