1eb7fdcd60e7cb53bcd0af81159a5b86703efd49
dcnoren <dcnoren@gmail.com>
2015-12-08 21:14:58 -0500

    humidity

--- task.php
+++ task.php
@@ -5,14 +5,14 @@ $getAllStatus = getAllStatus();
 
 print_r($getAllStatus);
 
-if (($getAllStatus["comfort"]["43"] - $getAllStatus["comfort"]["15"]) >= 10){
-	echo "43 is " . $getAllStatus["comfort"]["43"] . " and 15 is " . $getAllStatus["comfort"]["15"];
+if (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) >= 10){
+	echo "43 is " . $getAllStatus["comfort"]["43"]["Humidity"] . " and 15 is " . $getAllStatus["comfort"]["15"]["Humidity"];
 	setStatus("41", "On");
-	echo "41 on";
-} elseif (($getAllStatus["comfort"]["43"] - $getAllStatus["comfort"]["15"]) <= 10){
-	echo "43 is " . $getAllStatus["comfort"]["43"] . " and 15 is " . $getAllStatus["comfort"]["15"];
+	echo "</br>41 on";
+} elseif (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) <= 10){
+	echo "43 is " . $getAllStatus["comfort"]["43"]["Humidity"] . " and 15 is " . $getAllStatus["comfort"]["15"]["Humidity"];
 	setStatus("41", "Off");
-	echo "41 off";
+	echo "</br>41 off";
 }
 
 ?>
