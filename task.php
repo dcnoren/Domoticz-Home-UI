b93d58dd83d785edbc7f1b63e0624026da8217e9
dcnoren <dcnoren@gmail.com>
2015-12-08 21:12:45 -0500

    debug

--- task.php
+++ task.php
@@ -3,6 +3,8 @@ require('ajax/functions.php');
 
 $getAllStatus = getAllStatus();
 
+print_r($getAllStatus);
+
 if (($getAllStatus["comfort"]["43"] - $getAllStatus["comfort"]["15"]) >= 10){
 	echo "43 is " . $getAllStatus["comfort"]["43"] . " and 15 is " . $getAllStatus["comfort"]["15"];
 	setStatus("41", "On");
