b64adf3c3ab7a9006aefc8b6aa79fc3e1cf1b3fb
dcnoren <dcnoren@gmail.com>
2015-12-08 21:34:48 -0500

    15 percent humidity

--- task.php
+++ task.php
@@ -3,11 +3,11 @@ require('ajax/functions.php');
 
 $getAllStatus = getAllStatus("null", "array");
 
-if (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) >= 10){
+if (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) >= 15){
 	echo "43 is " . $getAllStatus["comfort"]["43"]["Humidity"] . " and 15 is " . $getAllStatus["comfort"]["15"]["Humidity"];
 	setStatus("41", "On");
 	echo "<br />41 on";
-} elseif (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) <= 10){
+} elseif (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) <= 15){
 	echo "43 is " . $getAllStatus["comfort"]["43"]["Humidity"] . " and 15 is " . $getAllStatus["comfort"]["15"]["Humidity"];
 	setStatus("41", "Off");
 	echo "<br />41 off";
