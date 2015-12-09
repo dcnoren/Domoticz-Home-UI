32fd7d49d2832c4891c3741b1d88e2c8576d3f26
dcnoren <dcnoren@gmail.com>
2015-12-08 21:32:24 -0500

    lines

--- task.php
+++ task.php
@@ -3,12 +3,6 @@ require('ajax/functions.php');
 
 $getAllStatus = getAllStatus("null", "array");
 
-//print_r ($getAllStatus);
-
-//echo "<br />";
-
-echo $getAllStatus["comfort"]["43"]["Humidity"];
-
 if (($getAllStatus["comfort"]["43"]["Humidity"] - $getAllStatus["comfort"]["15"]["Humidity"]) >= 10){
 	echo "43 is " . $getAllStatus["comfort"]["43"]["Humidity"] . " and 15 is " . $getAllStatus["comfort"]["15"]["Humidity"];
 	setStatus("41", "On");
